555a23406aa859513cac5d82987df2d200e23397
rosaliegrubb <rlg448@nyu.edu>
2024-03-13 15:39:25 +0800

    new changes

--- projectA/js/doodlebop.js
+++ projectA/js/doodlebop.js
@@ -2,6 +2,8 @@ let x;
 let y;
 let x1;
 let y1;
+let x3;
+let y3;
 let s = 50;
 let angle = 0;
 let x2 = [];
@@ -12,174 +14,228 @@ let speedX = [];
 let speedY = [];
 let drawingFinished = false; 
 let isBlack = false;
+let counter = 0;
+
+function increment() {
+    counter++;
+  }
 
 function setup() {
   let canvas = createCanvas(800, 500);
   canvas.parent("p5-canvas-container")
   colorMode(HSB, 100);
   resetSketch();
-}
-
-function resetSketch() {
-  x = width / 2;
-  y = 0;
-  x1 = width / 2;
-  y1 = random(0, height);
-  background('#19088B');
-  for (let i = 0; i < n; i++) {
-    x2[i] = random(width);
-    y2[i] = random(height);
-    s1[i] = random(80, 100);
-    speedX[i] = random(0.001, 0.01);
-    speedY[i] = random(0.001, 0.01);
-  }
-  drawingFinished = false;
-}
-
-function draw() {
-  if (!drawingFinished) {
-  noFill();
-  fill('black');
-  x1 = x1 + random(-frameCount * 1, frameCount * 1);
-  y1 = y1 + random(-frameCount * 1, frameCount * 1);
-  stroke(random(100), 50, 100);
-  noFill();
-  circle(x1, y1, random(1, 100));
-
-  x1 = x1 + random(-frameCount * 0.01, frameCount * 0.01);
-  y1 = y1 + random(-frameCount * 0.01, frameCount * 0.01);
-  stroke(random(100), 50, 100);
-  noFill();
-  circle(x1 + 200, y1 + 200, random(1, 40));
-
-  translate(0, 250);
-  for (let i = 0; i < width; i += s) {
-    for (let j = 0; j < height; j += s) {
-      fill('white');
-      rect(i, j, s, s);
-    }
-  }
-
-  //black squares
-  push();
-  translate(width / 2, height / 3);
-   if (isBlack) {
-  fill('black');
-  } else 
-  fill('green');
-  rotate(angle);
-  rect(0, 0, 30, 30);
-  pop();
-
-  push();
-  translate(width / 6, height / 10);
-  if (isBlack) {
-    fill('black');
-  } else 
-  fill('purple');
-  rotate(angle);
-  rect(0, 0, 30, 30);
-  pop();
-
-  push();
-  translate(width / 2, height / 6);
-  rotate(angle);
-    if (isBlack) {
-    fill('black');
-  } else 
-  fill('orange');
-  rect(0, 0, 50, 50);
-  pop();
-
-  push();
-  translate(width / 4, height / 4);
-  rotate(angle);
-  rectMode(CENTER);
-  if (isBlack) {
-    fill('black');
-  } else {
-    fill('red');
-  }
-  rect(0, 0, 100, 100);
-  angle += 0.05;
-  pop();
-
-  for (let i = 0; i < n; i++) {
-    drawDoodlebop(x2[i], y2[i], s1[i]);
-  }
-  move();
-}
-}
-function drawDoodlebop(x2, y2, s1) {
-  let x = map(sin(frameCount * 0.1), -1, 1, 0, 200);
-  let y = map(cos(frameCount * 0.1), -1, 1, 0, 200);
-  fill('lightblue');
-  push();
-  translate(x2 - 200, y2 - 100);
-  circle(0, 0, s1);
-  stroke('black');
-  arc(s1 * 0.02, s1 * 0.2, 18, 18, 0, PI);
-  fill("white");
-  circle(s1 * 0.02, s1 * -0.095, 50);
-  fill("black");
-  circle(s1 * 0.02, s1 * -0.05, 40);
-  fill("white");
-  circle(s1 * 0.02, s1 * -0.1, 20);
-  fill("white");
-  circle(s1 * 0.02, s1 * 0.1, 10);
-  fill("black");
-  //line(s1*0.5, s1*0.05, 30, 20, 30, 30);
-  fill("pink");
-  noStroke();
-  ellipse(s1 * -0.3, s1 * 0.15, 10, 7);
-  fill("pink");
-  noStroke();
-  ellipse(s1 * 0.3, s1 * 0.15, 10, 7);
-  fill('lightblue');
-  ellipse(s1 * 0.09, s1 * 0.5, 10, 20);
-  fill('lightblue');
-  ellipse(s1 * 0.03, s1 * 0.5, 10, 20);
-  pop();
-}
-
-
-function move() {
-  for (let i = 0; i < n; i++) {
-    if (mouseIsPressed) {
-      x2[i] = mouseX + 200; // Adjusted for translation
-      y2[i] = mouseY - 150; // Adjusted for translation
-    } else {
-      x2[i] = width * noise(frameCount * speedX[i]);
-      y2[i] = height * noise(frameCount * speedY[i]);
-    }
-  }
-}
-
-function mouseClicked() {
-  // Check for collisions with black squares
-  for (let i = 0; i < width; i += s) {
-    for (let j = 0; j < height; j += s) {
-      if (mouseX > i && mouseX < i + s && mouseY > j && mouseY < j + s) {
-        // Toggle isBlack variable for black square
-        if (isBlack) {
-          isBlack = false;
-          // Restore original color of Doodle Bop
-          fill('lightblue');
-        } else {
-          isBlack = true;
-          // Change Doodle Bop color to grey
-          fill(150); // Grey color
-        }
-        // Draw Doodle Bop with updated color
-        drawDoodlebop(x, y, s1);
-      }
-    }
-  }
-}
-
-
-function keyPressed() {
-  if (key === ' ') {
+  setInterval(increment, 2000); 
+  if (counter == 5){
     resetSketch();
   }
 }
+
+function resetSketch() {
+    x = width / 2;
+    y = 0;
+    x1 = width / 2;
+    y1 = random(0, height);
+    x3 = 10; // Initial x position of the sun
+    y3 = height / 2; // Initial y position of the sun
+    background('#19088B');
+    for (let i = 0; i < n; i++) {
+      x2[i] = random(width);
+      y2[i] = random(height);
+      s1[i] = random(80, 100);
+      speedX[i] = random(0.001, 0.01);
+      speedY[i] = random(0.001, 0.01);
+    }
+    drawingFinished = false;
+  }
+  
+  function draw() {
+    if (!drawingFinished) {
+    noFill();
+    fill('black');
+    x1 = x1 + random(-frameCount * 1, frameCount * 1);
+    y1 = y1 + random(-frameCount * 1, frameCount * 1);
+    stroke(random(100), 50, 100);
+    noFill();
+    circle(x1, y1, random(1, 100));
+  
+    x1 = x1 + random(-frameCount * 0.01, frameCount * 0.01);
+    y1 = y1 + random(-frameCount * 0.01, frameCount * 0.01);
+    stroke(random(100), 50, 100);
+    noFill();
+    circle(x1 + 200, y1 + 200, random(1, 40));
+  
+    translate(0, 250);
+    for (let i = 0; i < width; i += s) {
+      for (let j = 0; j < height; j += s) {
+        fill('white');
+        rect(i, j, s, s);
+      }
+    }
+  
+    //black squares
+    push();
+    translate(width / 2, height / 3);
+     if (isBlack) {
+    fill('black');
+    } else 
+    fill('green');
+    rotate(angle);
+    rect(0, 0, 30, 30);
+    pop();
+  
+    push();
+    translate(width / 6, height / 10);
+    if (isBlack) {
+      fill('black');
+    } else 
+    fill('purple');
+    rotate(angle);
+    rect(0, 0, 30, 30);
+    pop();
+  
+    push();
+    translate(width / 2, height / 6);
+    rotate(angle);
+      if (isBlack) {
+      fill('black');
+    } else 
+    fill('orange');
+    rect(0, 0, 50, 50);
+    pop();
+  
+    push();
+    translate(width / 4, height / 4);
+    rotate(angle);
+    rectMode(CENTER);
+    if (isBlack) {
+      fill('black');
+    } else {
+      fill('red');
+    }
+    rect(0, 0, 100, 100);
+    angle += 0.05;
+    pop();
+     
+    //sun
+      if (!isBlack) { // Only draw the sun if Doodle Bop is not grey
+        loop();
+        fill('yellow');
+        circle(x3, y3 - 100, 100);
+        x3 = (x3 + 1) % width; // Move the sun horizontally and wrap around
+        y3 = y + 1;
+      } else {
+        //noLoop(); // Stop drawing the sun if Doodle Bop is grey
+      }
+  
+    for (let i = 0; i < n; i++) {
+      drawDoodlebop(x2[i], y2[i], s1[i]);
+    }
+    move();
+  }
+  }
+  function drawDoodlebop(x2, y2, s1) {
+    let x = map(sin(frameCount * 0.1), -1, 1, 0, 200);
+    let y = map(cos(frameCount * 0.1), -1, 1, 0, 200);
+    if (isBlack==false){
+    fill('lightblue');
+    push();
+    translate(x2 - 200, y2 - 100);
+    circle(0, 0, s1);
+    stroke('black');
+    arc(s1 * 0.02, s1 * 0.2, 18, 18, 0, PI);
+    fill("white");
+    circle(s1 * 0.02, s1 * -0.095, 50);
+    fill("black");
+    circle(s1 * 0.02, s1 * -0.05, 40);
+    fill("white");
+    circle(s1 * 0.02, s1 * -0.1, 20);
+    fill("white");
+    circle(s1 * 0.02, s1 * 0.1, 10);
+    fill("black");
+    //line(s1*0.5, s1*0.05, 30, 20, 30, 30);
+    fill("pink");
+    noStroke();
+    ellipse(s1 * -0.3, s1 * 0.15, 10, 7);
+    fill("pink");
+    noStroke();
+    ellipse(s1 * 0.3, s1 * 0.15, 10, 7);
+    fill('lightblue');
+    ellipse(s1 * 0.09, s1 * 0.5, 10, 20);
+    fill('lightblue');
+    ellipse(s1 * 0.03, s1 * 0.5, 10, 20);
+    pop();
+    }else{
+    fill('gray');
+    push();
+    translate(x2 - 200, y2 - 100);
+    circle(0, 0, s1);
+    stroke('black');
+    arc(s1 * 0.02, s1 * 0.2, 18, 18, 0, PI);
+    fill("white");
+    circle(s1 * 0.02, s1 * -0.095, 50);
+    fill("black");
+    circle(s1 * 0.02, s1 * -0.05, 40);
+    fill("white");
+    circle(s1 * 0.02, s1 * -0.1, 20);
+    fill("white");
+    circle(s1 * 0.02, s1 * 0.1, 10);
+    fill("black");
+    //line(s1*0.5, s1*0.05, 30, 20, 30, 30);
+    fill("darkgray");
+    noStroke();
+    ellipse(s1 * -0.3, s1 * 0.15, 10, 7);
+    fill("darkgray");
+    noStroke();
+    ellipse(s1 * 0.3, s1 * 0.15, 10, 7);
+    fill('gray');
+    ellipse(s1 * 0.09, s1 * 0.5, 10, 20);
+    fill('gray');
+    ellipse(s1 * 0.03, s1 * 0.5, 10, 20);
+    pop();
+    }
+  }
+  
+  
+  function move() {
+    for (let i = 0; i < n; i++) {
+      if (mouseIsPressed) {
+        x2[i] = mouseX + 200; // Adjusted for translation
+        y2[i] = mouseY - 150; // Adjusted for translation
+      } else {
+        x2[i] = width * noise(frameCount * speedX[i]);
+        y2[i] = height * noise(frameCount * speedY[i]);
+      }
+    }
+  }
+  
+  function mouseClicked() {
+    // Check for collisions with black squares
+    for (let i = 0; i < width; i += s) {
+      for (let j = 0; j < height; j += s) {
+        if (mouseX > i && mouseX < i + s && mouseY > j && mouseY < j + s) {
+          // Toggle isBlack variable for black square
+          if (isBlack) {
+            isBlack = false;
+            // Restore original color of Doodle Bop
+            fill('lightblue');
+          } else {
+            isBlack = true;
+            // Change Doodle Bop color to grey
+            fill(150); // Grey color
+          }
+          // Draw Doodle Bop with updated color
+          drawDoodlebop(x, y, s1);
+        }
+      }
+    }
+  }
+  
+  
+  function keyPressed() {
+    if (key === ' ') {
+     resetSketch();
+    }
+    
+  }
+  
